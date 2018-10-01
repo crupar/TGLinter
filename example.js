551444f8d5f1a17138d45c69afa9ec891ec929a2
Carolyn Rupar <crupar@gmail.com>
2018-10-01 07:15:37 -0700

    changed example to directly go to index.js instead of folder

--- example.js
+++ example.js
@@ -1,16 +1,15 @@
-module.paths.push('/usr/local/lib/node_modules');
 
 var vfile = require('to-vfile');
 var report = require('vfile-reporter');
 var unified = require('unified');
 var english = require('retext-english');
 var stringify = require('retext-stringify');
-var tglinter = require('../truthgoggleslinter');
+var tglinter = require('./index.js');
 
 unified()
   .use(english)
   .use(tglinter)
   .use(stringify)
-  .process(vfile.readSync('example.txt'), function (err, file) {
-    console.error(report(err || file));
+  .process(vfile.readSync('example.html'), function (err, file) {
+    console.log(report(err || file));
   });
